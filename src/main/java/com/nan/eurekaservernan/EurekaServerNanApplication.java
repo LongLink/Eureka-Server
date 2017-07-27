961eae8f15fa1cf0d95a1dc537b58c6e557354c7
TobyLi0110 <1583946113@qq.com>
2017-07-27 14:21:41 +0800

    new project

---
+++ src/main/java/com/nan/eurekaservernan/EurekaServerNanApplication.java
@@ -0,0 +1,13 @@
+package com.nan.eurekaservernan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerNanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerNanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nan/eurekaservernan/EurekaServerNanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nan.eurekaservernan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaServerNanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
