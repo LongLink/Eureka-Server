95bb87a36255e4729fd6da779ee928179f9ab818
1583946113@qq.com <1583946113@qq.com>
2017-07-27 21:57:07 +0800

    test  merge

--- src/main/java/com/nan/eurekaservernan/EurekaServerNanApplication.java
+++ src/main/java/com/nan/eurekaservernan/EurekaServerNanApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaServerNanApplication {
 //testcherrypick
+//testmerge
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerNanApplication.class, args);
 	}
