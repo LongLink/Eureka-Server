4c787e277ed54f3aad993e7e565766a9e7863da7
1583946113@qq.com <1583946113@qq.com>
2017-07-27 21:05:31 +0800

    test source tree

--- src/main/java/com/nan/eurekaservernan/EurekaServerNanApplication.java
+++ src/main/java/com/nan/eurekaservernan/EurekaServerNanApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerNanApplication {
-
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerNanApplication.class, args);
 	}
